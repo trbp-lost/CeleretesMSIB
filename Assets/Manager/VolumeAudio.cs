318990892635b7342897e155631b41f36a854715
trbp-lost <vinotengku@gmail.com>
2024-05-19 04:32:24 +0700

    Add Volume Audio Slider
    ChangeLog :
    + add Volume Audio script
    + add new scene

---
+++ Assets/Manager/VolumeAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeAudio : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+
+    void Start()
+    {
+
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume(float vol)
+    {
+        AudioListener.volume = volumeSlider.value * 2;
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+    
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+}
