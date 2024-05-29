1190832c98747e1616df7a803a35507f458be231
trbp-lost <vinotengku@gmail.com>
2024-05-29 08:56:44 +0700

    Improve AI
    + Add patrol ,chase
    
    - facing wrong direction

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -4,29 +4,117 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform player; 
-    public float speed = 5f; 
-    // Start is called before the first frame update
+    public float patrolSpeed = 2f;
+    public float chaseSpeed = 4f;
+
+    public float attackRange = 1f;
+    public int patrolRange = 5;
+    public float chaselRange = 5f;
+    public float patrolDelay = 3f;
+    public Transform player;
+
+    private bool isChasing = false;
+    private bool isPatrolling = false;
+
+    private Rigidbody2D rb;
+    private Vector3 originalPosition;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        originalPosition = transform.position;
+        StartCoroutine(Patrolling());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Folllow();
-    }
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
-    private void Folllow()
-    {
-        if (player != null) 
+        if (distanceToPlayer < attackRange)
         {
-            Vector3 direction = player.position - transform.position;
-
-            direction.Normalize();
-
-            transform.position += direction * speed * Time.deltaTime;
+            AttackPlayer();
         }
+        
+        if (distanceToPlayer < chaselRange)
+        {
+            isChasing = true;
+            ChasePlayer();
+
+            CancelInvoke(nameof(Patrol));
+            //StopCoroutine(Patrolling());
+        }
+        else
+        {
+            if (isChasing)
+            {
+                isChasing = false;
+                rb.velocity = Vector2.zero; // Stop chasing when player is out of range
+            }
+
+            if (!isPatrolling)
+            {
+                StartCoroutine(Patrolling());
+            }
+        }
+        
+        //if (distanceToPlayer > chaselRange)
+        //{
+        //    //InvokeRepeating(nameof(Patrol), 0, patrolDelay);
+        //    Invoke(nameof(Patrol), patrolDelay);
+        //    //StartCoroutine(Patrolling());
+
+        //    isChasing = false;
+        //}
+
     }
+
+    void Patrol()
+    {
+        int randomX = Random.Range(-patrolRange, patrolRange);
+        if (transform.position.x > originalPosition.x + patrolRange || transform.position.x < originalPosition.x - patrolRange)
+        {
+            randomX = -randomX;
+        }
+        Debug.Log("rng =" + randomX + "\ntransform = " + transform.position.x);
+
+        Vector3 newDirection = new Vector3(randomX, transform.position.y, transform.position.z);
+        FaceDirection(0, randomX);
+
+        //transform.position = Vector3.MoveTowards(transform.position, newDirection, patrolSpeed * Time.deltaTime);
+        rb.velocity = new Vector2(newDirection.x * patrolSpeed, rb.velocity.y) ;
+    }
+
+    IEnumerator Patrolling()
+    {
+        isPatrolling = true;
+        while (!isChasing)
+        {
+            Patrol();
+            yield return new WaitForSeconds(patrolDelay);
+        }
+
+        isPatrolling = false;
+    }
+
+    void ChasePlayer()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+        Vector3 chaseLenght = transform.position + new Vector3(direction.x, direction.y, transform.position.z);
+        FaceDirection(transform.position.x, player.transform.position.x);
+
+        rb.velocity = new Vector2(direction.x * chaseSpeed, rb.velocity.y);
+    }
+
+    void AttackPlayer()
+    {
+        Debug.Log("Attacking the player!");
+    }
+
+    private void FaceDirection(float point, float target)
+    {
+        if (point > target) transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        if (point < target) transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+    }
+
 }
