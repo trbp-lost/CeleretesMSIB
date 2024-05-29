f2da9cf57544e14b021fa5771320e5c25e728c84
Saifalamsyah <adlisaifalamsyah@gmail.com>
2024-05-29 15:34:55 +0700

    Progress

---
+++ Part3_2D/Assets/Scripts1/carakunci1/PintuBiru/PintuBiru.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PintuBiru : MonoBehaviour
+{
+    public bool _isDoorOpen = false;
+    private Vector3 _doorClosedPos;
+    private Vector3 _doorOpenPos;
+    [SerializeField] private float _doorSpeed = 10f; // Buat serialize field untuk kecepatan pintu
+
+    void Awake()
+    {
+        _doorClosedPos = transform.position;
+        _doorOpenPos = new Vector3(transform.position.x, transform.position.y + 3f, transform.position.z);
+    }
+
+    void Update()
+    {
+        if (_isDoorOpen)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        if (transform.position != _doorOpenPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _doorOpenPos, _doorSpeed * Time.deltaTime);
+        }
+    }
+
+    void CloseDoor()
+    {
+        if (transform.position != _doorClosedPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _doorClosedPos, _doorSpeed * Time.deltaTime);
+        }
+    }
+}
